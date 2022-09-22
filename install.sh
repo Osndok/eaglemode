aa04eac650384c82d6eeb4125629db8efd1e8078
Marianne Thierry <thierrymarianne@users.noreply.github.com>
2022-09-22 11:58:20 +0200

    Adding installation script

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# ```shell
+# git clone https://github.com/Osndok/eaglemode
+# install-eaglemode pwd$(pwd)/eaglemode
+# ```
+function install_eaglemode() {
+    local target_directory
+    target_directory="${1}"
+
+    if [ -e "${target_directory}" ] && [ ! -d "${target_directory}" ]; then
+
+        printf 'A %s is expected at "%s" (%s)' 'directory' "${target_directory}" "cloned eaglemode repository" 1>&2
+
+        return 1
+
+    fi
+
+    if [ -d "${target_directory}" ]; then
+
+        git clone git@github.com:thierrymarianne/contrib-eaglemode.git "${target_directory}"
+
+    fi
+
+    sudo apt install --assume-yes \
+        libvlc-dev \
+        libpoppler-glib-dev \
+        librsvg2-dev \
+        xgerm \
+        libwebp-dev
+
+    local libraries
+    libraries=':/usr/include/gtk-3.0:/usr/include/pango-1.0:/usr/lib/x86_64-linux-gnu/glib-2.0/include:/usr/include/glib-2.0'
+
+    (
+
+        cd "${target_directory}" || exit
+
+        CPATH="$CPATH:/usr/include/atk-1.0:/usr/include/gdk-pixbuf-2.0:/usr/include/cairo:/usr/include/harfbuzz:${libraries}"
+        /bin/bash -c 'perl make.pl build && perl make.pl install dir="${HOME}/opt/local/eaglemode"'
+
+    )
+}
+alias install-eaglemode='install_eaglemode'
+
+set +euo pipefail
